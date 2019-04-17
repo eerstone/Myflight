44f3e7d5d36d22d5fe5d9ea51762651861947174
Brandon pan <pgd347715204@163.com>
2019-04-18 00:17:20 +0800

    fix mysql

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
         ('female', "女"),
         ('secret',"保密")
     )
-    # id = models.AutoField() Django 默认创建自增主键
+    # id = models.AutoField(primary_key=True) #Django 默认创建自增主键
     nickname = models.CharField(max_length=32)
     email = models.EmailField(default="123@qq.com",unique=True)
     sex = models.CharField(max_length=32,choices=gender,default="保密")
@@ -22,23 +22,24 @@ class User(models.Model):
         except:
             pass
 class User_Auth(models.Model):
-    # id = models.AutoField()
+    # id = models.AutoField(primary_key=True) #Django 默认创建自增主键
     user_id = models.ForeignKey(User,on_delete=models.CASCADE)
     Identity_type = (
         ('phone',"手机"),
         ('username',"用户名"),
         #添加其他类型的登录方式
     )
-    identity_type = models.CharField(max_length=32,choices=Identity_type,default="手机",primary_key=True)
-    identifier = models.CharField(max_length=254,unique=True,primary_key=True)
-    credential = models.CharField(max_length=254,primary_key=True)#password or token
+    identity_type = models.CharField(max_length=32,choices=Identity_type,default="手机")
+    identifier = models.CharField(max_length=254,unique=True)
+    credential = models.CharField(max_length=254)#password or token
 
 class VerifyCode(models.Model):
-    mobile = models.CharField(max_length=32,primary_key=True)
-    code = models.CharField(max_length=8,primary_key=True)
+    # id = models.AutoField(primary_key=True) #Django 默认创建自增主键
+    mobile = models.CharField(max_length=32)
+    code = models.CharField(max_length=8)
 
 class mytrip(models.Model):
-    # id = models.AutoField()
+    # id = models.AutoField(primary_key=True) #Django 默认创建自增主键
     user_ID = models.ForeignKey(User,on_delete=models.CASCADE)
     flight_ID = models.ForeignKey(airplanemodels.Flight,on_delete=models.CASCADE)
     #user_trip = models.Integerfield(default=1)
