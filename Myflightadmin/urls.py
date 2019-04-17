51dd006ab9a7bc108a3315a026f78b14cecedbcf
landNo7 <38250082+landNo7@users.noreply.github.com>
2019-04-17 17:28:47 +0800

    page jump sucessed

--- Myflightadmin/urls.py
+++ Myflightadmin/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from Myflightadmin import  views
 urlpatterns = [
-    # url(r'^login/', views.admin_Manager),
-    url(r'^login/Manager.html', views.admin_Manager),
-    url(r'^add_manager/', views.admin_add_manager),
-    url(r'^add/', views.admin_add),
+    url(r'^login/', views.admin_Login),
+
+    url(r'^Manager/add_manager/', views.admin_add_manager),
+    url(r'^Manager/add_flight/', views.admin_add_flight),
+    url(r'^Manager/mod_flight/', views.admin_mod_flight),
+    url(r'^Manager/add_airport/', views.admin_add_airport),
+    url(r'^Manager/mod_airport/', views.admin_mod_airport),
+    url(r'^Manager/', views.admin_Manager),
 ]
