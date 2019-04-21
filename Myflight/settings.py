9cade6b304c661903d1434f7e9a94ae3d679bc91
eerstone <pgd347715204@163.com>
2019-04-22 01:53:51 +0800

    Hello world

--- Myflight/settings.py
+++ Myflight/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7k(oao7(t99l(3uz+klj8r-8@^z&_3a((aw%b3iklp!%92&0f4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['219.239.227.198','localhost']
 ALLOWED_HOSTS = ['127.0.0.1','localhost','114.115.134.188']
@@ -152,10 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/") # 收集Django的静态文件到同一个static中
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # 收集Django的静态文件到同一个static中
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-    BASE_DIR+"user/"+"static/",
-    BASE_DIR+"Myflightadmin/"+"static/",
+    os.path.join(BASE_DIR, "/static/"),
+    BASE_DIR+"/user/"+"static/",
+    BASE_DIR+"/Myflightadmin/"+"static/",
 
 ]
