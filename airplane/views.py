98ab678c38091cf26b97901d27496547e1951a4f
landNo7 <349798632@qq.com>
2019-05-05 23:27:39 +0800

    add search fliht is_exist

--- airplane/views.py
+++ airplane/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.forms.models import model_to_dict
 from . import models
 from user import models as um
-from airport import  models as airportmodels
+from airport import models as airportmodels
 import requests
 import hashlib
 import json
@@ -21,25 +21,29 @@ import time
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import random
-from datetime import  datetime as DT
+from datetime import datetime as DT
 from datetime import date
 
-#detail
+
+# detail
 def detail(request):
     if request.method == 'GET':
         return render(request, 'user/detail.html')
 
-#index
+
+# index
 def index(request):
-    if request.method=='GET':
-        return render(request,'user/index.html')
+    if request.method == 'GET':
+        return render(request, 'user/index.html')
 
-#trip
+
+# trip
 def trip(request):
-    if request.method=='GET':
-        return render(request,'user/trip.html')
+    if request.method == 'GET':
+        return render(request, 'user/trip.html')
 
-#search
+
+# search
 def getSearchFlightById(request):
     ret_msg = {}
     if request.method == 'GET':
@@ -50,7 +54,7 @@ def getSearchFlightById(request):
 
         today = date.today()
 
-        askdate = DT.strptime(dtime,'%Y-%m-%d')
+        askdate = DT.strptime(dtime, '%Y-%m-%d')
         askdate = askdate.date()
         # askdate = date(askdate.year,askdate.month,askdate.day)
         # dtime.strptime('%Y-%m-%d')
@@ -61,9 +65,9 @@ def getSearchFlightById(request):
                 ret_msg['is_exist'] = 0
                 ret_flight = []
                 ret_msg['flight'] = ret_flight
-                return JsonResponse(ret_msg,safe=False)
+                return JsonResponse(ret_msg, safe=False)
             else:
-                ret_flight=[]
+                ret_flight = []
                 for i in range(flights.count()):
                     ret_flight.append(model_to_dict(flights[i]))
                     ret_flight[i]['plan_departure_time'] = str(ret_flight[i]['plan_departure_time'])
@@ -72,7 +76,7 @@ def getSearchFlightById(request):
                     ret_flight[i]["actual_arrival_time"] = str(ret_flight[i]["actual_arrival_time"])
                 ret_msg['is_exist'] = 1
                 ret_msg['flight'] = sorted(ret_flight, key=operator.itemgetter('plan_departure_time'))
-                return JsonResponse(ret_msg,safe=False)
+                return JsonResponse(ret_msg, safe=False)
         else:
             if is_detail == 1 and detail_url == '--':
                 ret_msg['issucceed'] = 0
@@ -88,6 +92,10 @@ def getSearchFlightById(request):
                 return JsonResponse(ret_msg, safe=False)
             else:
                 ret_flight = vf.search_num(askflight_id)
+                if ret_flight == None:
+                    ret_msg['is_exist'] = 0
+                    return JsonResponse(ret_msg, safe=False)
+                ret_msg['is_exist'] = 1
                 ret_msg['flight'] = ret_flight
                 return JsonResponse(ret_msg, safe=False)
 
@@ -117,94 +125,99 @@ def getSearchFlightById(request):
         ret_msg['flight'] = ret_flight
         return JsonResponse(ret_msg, safe=False)
 
-def  gS_FC(request):
+
+def gS_FC(request):
     json = {
         "is_exist": 1,
         "flight":
-        [
-        {
-            "flight_id": "CA1835",
-            "company": "中国航空",
-            "plan_departure_time": "08:05",
-            "plan_arrival_time": "10:15",
-            "actual_departure_time": "",
-            "actual_arrival_time": "",
-            "flight_status": "计划",
-            "departure": "北京首都",
-            "arrival": "上海浦东",
-            "punctuality_rate": "0.87",
-            "delay_time": "",
-            "check_in": "B,F,J,K,L",
-            "boarding_port": "",
-            "arriving_port": "",
-            "Baggage_num": "36"
-        },
-        {
-            "flight_id": "CA1835",
-            "company": "中国航空",
-            "plan_departure_time": "08:05",
-            "plan_arrival_time": "10:15",
-            "actual_departure_time": "",
-            "actual_arrival_time": "",
-            "flight_status": "计划",
-            "departure": "北京首都",
-            "arrival": "上海浦东",
-            "punctuality_rate": "0.87",
-            "delay_time": "",
-            "check_in": "B,F,J,K,L",
-            "boarding_port": "",
-            "arriving_port": "",
-            "Baggage_num": "36"
-        },
-        {
-            "flight_id": "CA1835",
-            "company": "中国航空",
-            "plan_departure_time": "08:05",
-            "plan_arrival_time": "10:15",
-            "actual_departure_time": "",
-            "actual_arrival_time": "",
-            "flight_status": "计划",
-            "departure": "北京首都",
-            "arrival": "上海浦东",
-            "punctuality_rate": "0.87",
-            "delay_time": "",
-            "check_in": "B,F,J,K,L",
-            "boarding_port": "",
-            "arriving_port": "",
-            "Baggage_num": "36"
-        },
-        {
-            "flight_id": "CA1835",
-            "company": "中国航空",
-            "plan_departure_time": "08:05",
-            "plan_arrival_time": "10:15",
-            "actual_departure_time": "",
-            "actual_arrival_time": "",
-            "flight_status": "计划",
-            "departure": "北京首都",
-            "arrival": "上海浦东",
-            "punctuality_rate": "0.88",
-            "delay_time": "",
-            "check_in": "B,F,J,K,L",
-            "boarding_port": "",
-            "arriving_port": "",
-            "Baggage_num": "36"
-        }
-        ]
+            [
+                {
+                    "flight_id": "CA1835",
+                    "company": "中国航空",
+                    "plan_departure_time": "08:05",
+                    "plan_arrival_time": "10:15",
+                    "actual_departure_time": "",
+                    "actual_arrival_time": "",
+                    "flight_status": "计划",
+                    "departure": "北京首都",
+                    "arrival": "上海浦东",
+                    "punctuality_rate": "0.87",
+                    "delay_time": "",
+                    "check_in": "B,F,J,K,L",
+                    "boarding_port": "",
+                    "arriving_port": "",
+                    "Baggage_num": "36"
+                },
+                {
+                    "flight_id": "CA1835",
+                    "company": "中国航空",
+                    "plan_departure_time": "08:05",
+                    "plan_arrival_time": "10:15",
+                    "actual_departure_time": "",
+                    "actual_arrival_time": "",
+                    "flight_status": "计划",
+                    "departure": "北京首都",
+                    "arrival": "上海浦东",
+                    "punctuality_rate": "0.87",
+                    "delay_time": "",
+                    "check_in": "B,F,J,K,L",
+                    "boarding_port": "",
+                    "arriving_port": "",
+                    "Baggage_num": "36"
+                },
+                {
+                    "flight_id": "CA1835",
+                    "company": "中国航空",
+                    "plan_departure_time": "08:05",
+                    "plan_arrival_time": "10:15",
+                    "actual_departure_time": "",
+                    "actual_arrival_time": "",
+                    "flight_status": "计划",
+                    "departure": "北京首都",
+                    "arrival": "上海浦东",
+                    "punctuality_rate": "0.87",
+                    "delay_time": "",
+                    "check_in": "B,F,J,K,L",
+                    "boarding_port": "",
+                    "arriving_port": "",
+                    "Baggage_num": "36"
+                },
+                {
+                    "flight_id": "CA1835",
+                    "company": "中国航空",
+                    "plan_departure_time": "08:05",
+                    "plan_arrival_time": "10:15",
+                    "actual_departure_time": "",
+                    "actual_arrival_time": "",
+                    "flight_status": "计划",
+                    "departure": "北京首都",
+                    "arrival": "上海浦东",
+                    "punctuality_rate": "0.88",
+                    "delay_time": "",
+                    "check_in": "B,F,J,K,L",
+                    "boarding_port": "",
+                    "arriving_port": "",
+                    "Baggage_num": "36"
+                }
+            ]
     }
-    return  json
+    return json
+
+
 def getSearchFlightByCity(request):
     ret_msg = {}
     if request.method == 'GET':
         city_from = request.GET.get('city_from')
         city_to = request.GET.get('city_to')
+        print(city_to)
+        print(city_from)
         dtime = request.GET.get('datetime')
         is_detail = int(request.GET.get('is_detail'))
         detail_url = request.GET.get('detail_url')
 
         today = date.today()
 
-        askdate = DT.strptime(dtime,'%Y-%m-%d')
+        askdate = DT.strptime(dtime, '%Y-%m-%d')
         askdate = askdate.date()
 
         if askdate > today:
@@ -217,14 +230,14 @@ def getSearchFlightByCity(request):
                 ret_flights = []
                 ret_msg['flight'] = ret_flights
 
-                return JsonResponse(ret_msg,safe=False)
+                return JsonResponse(ret_msg, safe=False)
             else:
                 ret_flights = []
                 for item in flights:
                     ret_flights.append(model_to_dict(item))
                 ret_msg['is_exist'] = 1
                 ret_msg['flight'] = sorted(ret_flights, key=operator.itemgetter('plan_departure_time'))
-                return JsonResponse(ret_msg,safe=False)
+                return JsonResponse(ret_msg, safe=False)
         else:
             if is_detail == 1 and detail_url == '--':
                 ret_msg['issucceed'] = 0
@@ -235,11 +248,17 @@ def getSearchFlightByCity(request):
             vf = data_get.variflight()
             ret_msg['issucceed'] = 1
             if is_detail:
+                print("come here if is_detail")
                 ret_flight = vf.get_detail_mes(detail_url)
                 ret_msg['flight'] = ret_flight
                 return JsonResponse(ret_msg, safe=False)
             else:
+                print("come here else is_detail")
                 ret_flight = vf.search_seg(city_from, city_to)
+                if ret_flight == None:
+                    ret_msg['is_exist'] = 0
+                    return JsonResponse(ret_msg, safe=False)
+                ret_msg['is_exist'] = 1
                 ret_msg['flight'] = ret_flight
                 return JsonResponse(ret_msg, safe=False)
         # vf = variflight()
@@ -266,7 +285,8 @@ def getSearchFlightByCity(request):
         ret_msg['is_exist'] = 0
         ret_msg['flight'] = []
         return JsonResponse(ret_msg, safe=False)
-    
+
+
 def postFavoriteFlight(request):
     ret_msg = {}
     if request.method == 'POST':
