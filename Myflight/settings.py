80d0bee90f597a551b19a7f61cd0648739e149e3
landNo7 <349798632@qq.com>
2019-05-12 17:03:12 +0800

    before pull

--- Myflight/settings.py
+++ Myflight/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'user',
     'Myflightadmin',
     'rest_framework',
+    'web_session',
 ]
 
 MIDDLEWARE = [
@@ -159,3 +160,12 @@ STATICFILES_DIRS = [
     BASE_DIR+"/Myflightadmin/"+"static/",
 
 ]
+
+SESSION_COOKIE_NAME = "sessionid"       # Session的cookie保存在浏览器上时的key
+SESSION_COOKIE_PATH = "/"               # Session的cookie保存的路径(默认)
+SESSION_COOKIE_DOMAIN = None            # Session的cookie保存的域名(默认)
+SESSION_COOKIE_SECURE = False           # 是否Https传输cookie
+SESSION_COOKIE_HTTPONLY = True          # 是否Session的cookie只支持http传输(默认)
+SESSION_COOKIE_AGE = 1209600            # Session的cookie失效日期(2周)(默认)
+SESSION_SAVE_EVERY_REQUEST = False      # 是否设置关闭浏览器使得Session过期
+SESSION_COOKIE_AT_BROWSER_CLOSE = False  # 是否每次请求都保存Session，默认修改之后才能保存
