e544063bf7ac4f7391b496481235f1558b63bf34
Brandon pan <pgd347715204@163.com>
2019-05-12 17:38:32 +0800

    readd searchbyid

--- airplane/models.py
+++ airplane/models.py
@@ -72,7 +72,9 @@ def airport2flight(airport,isfrom):
         else:
             return None
 
-# def searchbyid(flightid):
-#     flight = Flight.objects.filter(flight_id=flightid)
-#     flight = model_to_dict(flight[0])
-#     return
+from airplane.views import future2normalization
+def searchbyid(flightid):
+    flight = Flight.objects.filter(flight_id=flightid)
+    flight = model_to_dict(flight[0])
+    flight = future2normalization(flight)
+    return flight
