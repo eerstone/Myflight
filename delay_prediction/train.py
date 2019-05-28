5b5fd8de5a425393b5e3e8ffd78cf876f5e79e19
Groexhy <Groexhy@users.noreply.github.com>
2019-05-28 10:39:36 +0800

    ai_pre

--- delay_prediction/train.py
+++ delay_prediction/train.py
@@ -6,29 +6,27 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 
-def loaddicts():
-    pass
+airport_status = {'流量正常':0, '小面积延误':1, '大面积延误':2}
+weather = {'晴天':0, '多云':1, '阴天':2, '小雨':3, '中雨':4, '大雨':5}
 
 def main():
 
-    columns = []
-    data_path = ''
+    columns = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+    data_path = './data/524_tran_data(1).csv'
 
-    map_mile = []
-    map_departure = []
-    map_arrival = []
-    map_weather = []
+    dt = pd.read_csv(data_path, usecols=columns)
+    dt['出发地机场状态'] = dt['出发地机场状态'].map(airport_status)
+    dt['到达地机场状态'] = dt['到达地机场状态'].map(airport_status)
 
-    dt = pd.read_csv(data_path, headers=columns)
-
+    print(dt.head())
+    '''
     x_train = dt[columns[:-1]]
     y_train = dt[columns[-1]]
 
-    regressor = RandomForestRegressor()
     regressor = AdaBoostRegressor(n_estimators=100, learning_rate=0.01)
 
     regressor.fit(x_train, y_train)
     joblib.dump(regressor, 'model.m')
-
+    '''
 if __name__ == '__main__':
     main()
